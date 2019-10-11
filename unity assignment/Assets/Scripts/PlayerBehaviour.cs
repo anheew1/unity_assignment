e0a9378be4800b1c2944f0723b5dcc123cb19cf5
anheew1 <anheew1@kookmin.ac.kr>
2019-10-12 00:34:34 +0900

    add function getMouseWorldPos

--- unity assignment/Assets/Scripts/PlayerBehaviour.cs
+++ unity assignment/Assets/Scripts/PlayerBehaviour.cs
@@ -22,22 +22,24 @@ public class PlayerBehaviour : MonoBehaviour
         Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
         coordZ = playerScreenPos.z;
         coordY = playerScreenPos.y;
-        Vector3 mousePos =Input.mousePosition;
-        mousePos.z = coordZ;
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        
 
-        offset = transform.position - mouseWorldPos;
+        offset = transform.position - getMouseWorldPos();
         
     }
-    
-    // Update is called once per frame
-    private void OnMouseDrag()
+    private Vector3 getMouseWorldPos()
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = coordZ;
         mousePos.y = coordY;
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
-        transform.position = mouseWorldPos + offset;
+        return mouseWorldPos;
+    }
+    
+    // Update is called once per frame
+    private void OnMouseDrag()
+    {
+        transform.position = getMouseWorldPos() + offset;
         
     }
 
